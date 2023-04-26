a2159493b890fdab7623d8e7dc36d15d801e0e2f
aliawada <awadinha99@gmail.com>
2023-04-25 23:43:08 -0300

    criação das páginas do desafio com tabelas dummys

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,49 +1,17 @@
+import Navbar from '@/components/navbar'
 import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
-        <title>File uploader</title>
-        <meta name="description" content="File uploader" />
+        <title>Hubla</title>
+        <meta name="description" content="Hubla" />
       </Head>
-
+      <Navbar />
       <main>
-
-
-        <nav>
-          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="" className="flex items-center">
-              <img src="6206aa98efa43a84b2d43f98_HUBLA Logo.svg" className="h-8 mr-3" alt="Hubla Logo" />
-            </a>
-            <div className="flex md:order-2">
-              <button type="button" className="text-white bg-gray-700 font-medium rounded-md text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-800">Upload File</button>
-            </div>
-            <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-              <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
-                <li>
-                  <a href="#" className="group  transition duration-300">
-                    Creators
-                    <span className="block max-w-0 group-hover:max-w-full transition-all duration-400 h-0.5 bg-gray-900"></span>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="group  transition duration-300">
-                    Affiliates
-                    <span className="block max-w-0 group-hover:max-w-full transition-all duration-400 h-0.5 bg-gray-900"></span>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="group  transition duration-300">
-                    Transactions
-                    <span className="block max-w-0 group-hover:max-w-full transition-all duration-400 h-0.5 bg-gray-900"></span>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        Index
       </main>
-    </div>
+    </>
   )
 }
---
+++ frontend/pages/transactions.tsx
@@ -0,0 +1,92 @@
+import Navbar from '@/components/navbar'
+import Head from 'next/head'
+
+export default function Transactions() {
+    return (
+        <>
+            <Head>
+                <title>Transactions</title>
+                <meta name="description" content="Transactions" />
+            </Head>
+            <Navbar />
+            <main>
+                <div className="relative overflow-x-auto">
+                    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" className="px-6 py-3">
+                                    Type
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Date
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Product
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Value
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Seller
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    Apple MacBook Pro 17"
+                                </th>
+                                <td className="px-6 py-4">
+                                    $2999
+                                </td>
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    Apple MacBook Pro 17"
+                                </th>
+                                <td className="px-6 py-4">
+                                    $2999
+                                </td>
+                                <td className="px-6 py-4">
+                                    $2999
+                                </td>
+                            </tr>
+                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    Microsoft Surface Pro
+                                </th>
+                                <td className="px-6 py-4">
+                                    $1999
+                                </td>
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    Apple MacBook Pro 17"
+                                </th>
+                                <td className="px-6 py-4">
+                                    $2999
+                                </td>
+                                <td className="px-6 py-4">
+                                    $2999
+                                </td>
+                            </tr>
+                            <tr className="bg-white dark:bg-gray-800">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    Magic Mouse 2
+                                </th>
+                                <td className="px-6 py-4">
+                                    $99
+                                </td>
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    Apple MacBook Pro 17"
+                                </th>
+                                <td className="px-6 py-4">
+                                    $2999
+                                </td>
+                                <td className="px-6 py-4">
+                                    $2999
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </main>
+        </>
+    )
+}
